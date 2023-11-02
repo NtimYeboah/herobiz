bbd41d71860e388d76beec7214d1f08502c7394c
Ntim Yeboah <lylegendary@gmail.com>
2023-11-02 13:56:30 +0000

    Implement search functionality

---
+++ search.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying search results.
+ */
+get_header();
+?>
+
+<div id="primary" class="search-content-area">
+    <main id="main" class="site-main">
+        <?php
+            if (have_posts()):
+                while (have_posts()):
+                    the_post();
+                    get_template_part('template-parts/page/content', 'search');
+                endwhile;
+
+                echo paginate_links([
+                    'prev_text' => esc_html__('Prev', 'herobiz'),
+                    'next_text' => esc_html__('Next', 'herobiz'),
+                ]);
+            else:
+                get_template_part('template-parts/page/content', 'none');
+            endif;
+        ?>
+    </main>
+</div>
