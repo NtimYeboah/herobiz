56484167426901664d492e6d69af575c1ff9f6ce
Ntim Yeboah <lylegendary@gmail.com>
2023-11-06 16:34:32 +0000

    Use theme to implement displaying of posts

--- template-parts/post/content.php
+++ template-parts/post/content.php
@@ -3,40 +3,72 @@
  * Templae part for displaying posts.
  */
 ?>
-<article id="post-<?php the_ID() ?>">
-    <header>
-        <?php
-            if (is_singular()):
-                the_title('<h2 class="entry-title">', '</h2>');
-            else:
-                the_title('<h2 class="entry-title"><a class="entry-link" href="'.esc_url(get_permalink()).'">', '</a></h2>');
-            endif;
-        ?>
-    </header>
 
-    <!-- Post thumbnail -->
-    <?php
-        if (has_post_thumbnail()):
-            the_post_thumbnail('large'); // full, large, medium
-        endif;
-    ?>
-
-    <!-- Post Content -->
-    <?php if (is_home() || is_archive()): ?> <!-- Show excerpt in home or archive pages -->
-        <div class="entry-content">
-            <?php the_excerpt() ?>
-        </div>
-    <?php elseif (is_single()): ?> <!--Show full conent in single page -->
-        <div class="entry-content">
+<div class="col-lg-6" id="post-<?php the_ID() ?>">
+    <article class="d-flex flex-column">
+        <div class="post-img">
             <?php
-                the_content();
-
-                wp_link_pages([
-                    'before' => 'div class="page-links"' . esc_html__('Pages:', 'ninestars'),
-                    'after' => '</div>',
-                ]);
+            if (has_post_thumbnail()):
+                the_post_thumbnail('large'); // full, large, medium
+            endif;
             ?>
         </div>
-    <?php endif ?>
-    
-</article>
+
+        <h2 class="title">
+            <?php
+            if (is_singular()):
+                the_title('<span>', '</span>');
+            else:
+                the_title('<a href="'.esc_url(get_permalink()).'">', '</a>');
+            endif;
+            ?>
+        </h2>
+
+        <div class="meta-top">
+            <ul>
+                <li class="d-flex align-items-center">
+                    <i class="bi bi-person"></i>
+                        <a href="blog-details.html">
+                            <?php the_author(); ?>
+                        </a>
+                </li>
+                <li class="d-flex align-items-center">
+                    <i class="bi bi-clock"></i>
+                        <a href="blog-details.html">
+                            <?php echo get_the_date(); ?>
+                        </a>
+                </li>
+                <?php if (have_comments()): ?>
+                    <li class="d-flex align-items-center">
+                        <i class="bi bi-chat-dots"></i>
+                            <a href="blog-details.html">
+                                <?php get_comments_number(); ?>
+                            </a>
+                    </li>
+                <?php endif; ?>
+            </ul>
+        </div>
+
+        <div class="content">
+            <?php if (is_home() || is_archive()): ?> <!-- Show excerpt in home or archive pages -->
+                <p><?php the_excerpt(); ?></p>
+            <?php elseif (is_single()): ?> <!--Show full conent in single page -->
+                <p>
+                    <?php
+                    the_content();
+
+                    /* wp_link_pages([
+                        'before' => 'div class="page-links"' . esc_html__('Pages:', 'ninestars'),
+                        'after' => '</div>',
+                    ]); */
+                    ?>
+                </p>
+            <?php endif; ?>
+        </div>
+
+        <div class="read-more mt-auto align-self-end">
+            <?php printf('<a href="%1$s">Read More</a>', esc_url(get_permalink())); ?>
+        </div>
+
+    </article>
+</div><!-- End post list item -->
